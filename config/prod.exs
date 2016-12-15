0299477703ecd107eb3ca2d8ba517730c2c00f29
Justin Feng <justin.feng@myob.com>
2016-12-16 10:55:25 +1100

    get rid of force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,6 @@ use Mix.Config
 # which you typically run after static files are built.
 config :price_crawler, PriceCrawler.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "product-price-crawler.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
